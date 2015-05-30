474b27746507a73daf0f089a0a498d466ec2c940
Luciano Donati <luciano.donati536@gmail.com>
2015-05-30 04:18:44 -0400

    SoundPlayer.cs added. [How to use] Attach it to whatever object you need to play a sound, fill in the list, name it and attach audio clips. Then, whenever you want to play a sound call GetComponent<SoundPlayer>().PlaySound("SOUND NAME HERE");

---
+++ Arena/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Sound
+    {
+        public string name;
+        public List<AudioClip> clips;
+    }
+
+    private AudioSource audioSource;
+
+    public List<Sound> sounds;
+
+    // Use this for initialization
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlaySound(string _sound)
+    {
+        audioSource.clip = getRandomClip(_sound);
+        if (audioSource.clip == null)
+            Debug.LogError("Trying to play: " + _sound + " ,but is not assigned.");
+        else
+            audioSource.Play();
+    }
+
+    private AudioClip getRandomClip(string _sound)
+    {
+        AudioClip theClip = null;
+        foreach (Sound theSound in sounds)
+        {
+            if (theSound.name.Equals(_sound))
+                theClip = theSound.clips[Random.Range(0, theSound.clips.Count)];
+        }
+        return theClip;
+    }
+}
