6bce0502c0f830559d499e09d2b3b691d0f86d60
Brian <Shinjo852@yahoo.com>
2015-05-29 22:52:12 -0400

    w/e

--- Arena/Assets/Scripts/PlayerController.cs
+++ Arena/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float playerSpeed = 15.0f;
+    public float playerSpeed = 150.0f;
     public float playerVelocityX;
     public float playerVelocityZ;
 
@@ -27,16 +27,6 @@ public class PlayerController : MonoBehaviour
 
         PRB.velocity = new Vector3(playerVelocityX, 0, playerVelocityZ);
 
-        //Vector3 move = new Vector3(playerVelocity, 0, 0);
-        //PRB.MovePosition(move);
-
-        print(Input.GetAxisRaw("Horizontal"));
-        //float h = Input.GetAxisRaw("Horizontal");
-       // float xPos = h * playerSpeed;
-        
-        //transform.position = new Vector3(xPos, 0, 2.0f);
-
-      //  textOutput.text = "Value Returned: "+h.ToString("F2");  
-      //  GetComponent<Rigidbody>().velocity = ;
+       
     }
 }
