44200209eaa8c1f01989e3d6f316e3e15ffc1dbc
Plaxius <GameDev28@gmail.com>
2015-05-29 23:12:45 -0400

    Added Projectile.cs and Projectile_Spawner.cs

---
+++ Arena/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed = 10;
+	Vector3 vel = new Vector3(1,1,1);
+	Vector3 pos = new Vector3(0,0,0);
+	public AudioClip sound;
+	public Player owner;
+	Projectile_Spawner launcher;
+	public Rigidbody RB;
+
+
+	// Use this for initialization
+	void Start () {
+
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( RB.velocity.magnitude <= 15 )
+		{
+			RB.AddForce( RB.velocity, ForceMode.VelocityChange );	
+		}
+
+	
+	}
+}
---
+++ Arena/Assets/Scripts/Projectile_Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_Spawner : MonoBehaviour {
+
+	public Projectile fireball;
+	public AudioClip launchSound;
+	float gameTime;
+	public Vector2 projectileVelocity;
+	bool canFire;
+	Player owner;
+	Rigidbody RB;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(canFire)
+			{
+				Projectile fireball = (GameObject)Instantiate(Projectile, transform.position + transform.forward * 2, transform.rotation);
+				fireball.RB.velocity = Vector2(1.0f, 0.0f);
+				fireball.owner = owner;
+			}
+
+	
+		}
+
+	
+	}
+}
