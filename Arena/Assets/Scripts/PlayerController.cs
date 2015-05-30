f1807705a4f2789cf46465b8ef364505df964c4d
Luciano Donati <luciano.donati536@gmail.com>
2015-05-29 22:51:41 -0400

    Updates

--- Arena/Assets/Scripts/PlayerController.cs
+++ Arena/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed = 15.0f;
+    public float playerVelocityX;
+    public float playerVelocityZ;
+
+    public Rigidbody PRB;
+
     // Use this for initialization
     private void Start()
     {
@@ -11,5 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        playerVelocityX = playerSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        playerVelocityZ = playerSpeed * Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+        PRB.velocity = new Vector3(playerVelocityX, 0, playerVelocityZ);
     }
 }
