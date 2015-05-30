b4c5fffb5b231ba55c3bdbec30a8feb8229f6e1d
Luciano Donati <luciano.donati536@gmail.com>
2015-05-29 23:45:56 -0400

    Updates

--- Arena/Assets/Scripts/Projectile_Spawner.cs
+++ Arena/Assets/Scripts/Projectile_Spawner.cs
@@ -1,36 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class Projectile_Spawner : MonoBehaviour {
+public class Projectile_Spawner : MonoBehaviour
+{
+    public Projectile fireball;
+    public AudioClip launchSound;
+    private float gameTime;
+    public Vector2 projectileVelocity;
+    private bool canFire;
+    private Player owner;
+    private Rigidbody RB;
 
-	public Projectile fireball;
-	public AudioClip launchSound;
-	float gameTime;
-	public Vector2 projectileVelocity;
-	bool canFire;
-	Player owner;
-	Rigidbody RB;
+    // Use this for initialization
+    private void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			if(canFire)
-			{
-				Projectile fireball = (GameObject)Instantiate(Projectile, transform.position + transform.forward * 2, transform.rotation);
-				fireball.RB.velocity = Vector2(1.0f, 0.0f);
-				fireball.owner = owner;
-			}
-
-	
-		}
-
-	
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canFire)
+            {
+                fireball.RB.velocity = new Vector3(1.0f, 0.0f);
+                fireball.owner = owner;
+            }
+        }
+    }
 }
