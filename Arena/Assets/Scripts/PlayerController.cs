f3eea361732e427fd170e9bf6aeae450bdd50d9f
Luciano Donati <luciano.donati536@gmail.com>
2015-05-29 22:48:12 -0400

    Lava done, Player updated

---
+++ Arena/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
