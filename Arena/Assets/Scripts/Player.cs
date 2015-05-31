4975e97c78a640f5ef7c94363d98cef124c6a6c1
Brian <Shinjo852@yahoo.com>
2015-05-31 08:08:41 -0400

    jnmkjnhikjnh,ikjnhikjnhikhik

--- Arena/Assets/Scripts/Player.cs
+++ Arena/Assets/Scripts/Player.cs
@@ -60,7 +60,7 @@ public class Player : MonoBehaviour
             enemy = GameObject.Find("Player 1").GetComponent<Player>();
 
         GameObject.Find("P" + id.ToString() + "_img").GetComponent<Image>().color = myColor;
-        GameObject.Find("PlayerShowdown" + id.ToString()).GetComponent<SpriteRenderer>().color = myColor;
+        //GameObject.Find("PlayerShowdown" + id.ToString()).GetComponent<SpriteRenderer>().color = myColor;
     }
 
     // Update is called once per frame
@@ -110,17 +110,7 @@ public class Player : MonoBehaviour
         takeDamage(50.0f);
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        GameObject collGO = collision.gameObject;
-    }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        GameObject collGO = collision.gameObject;
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
