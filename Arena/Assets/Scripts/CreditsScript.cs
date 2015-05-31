1ee9864f28706940218728325ed2df0f78d669df
Plaxius <GameDev28@gmail.com>
2015-05-31 07:33:00 -0400

    Added Credits Menu

---
+++ Arena/Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class CreditsScript : MonoBehaviour {
+	
+	public float creditSpeed = 10; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		pos.y += creditSpeed * Time.deltaTime;
+		transform.position = pos;
+	
+	}
+	
+}
